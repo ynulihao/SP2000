f685e15862b93e4c5a4e710dba3b07ff6b16f700
lihao <lihao@mail.ynu.edu.cn>
2020-08-02 23:29:05 +0800

    apply v2 api

--- SP2000/sp2000.py
+++ SP2000/sp2000.py
@@ -19,44 +19,99 @@ class SP2000:
         """
         self.key = key
 
-    def search_family_id(self, *queries):
+        # checkAPI
+        data = json.loads(requests.get('http://www.sp2000.org.cn/api/v2/getFamiliesByFamilyName',
+                                       params={'familyName': 'Cyprinidae', 'apiKey': self.key,
+                                               'page': 1}).text)
+        assert data['code'] == '401', 'Please check your apiKey.'
+
+    def search_family_id(self, *queries, start=1, limit=20):
         """Search family IDs
         Search family IDs via family name, supports Latin and Chinese names.
         :param queries:Family name, or part of family name, supports Latin and Chinese names. Single or more query.
+        :param start:intenger Record number to start at. If omitted, the results are returned from the first record
+            (start=1). Use in combination with limit to page through results. Note that we do the paging internally for
+            you, but you can manually set the start parameter.
+        :param limit: intenger Number of records to return. This is passed across all sources,when you first query,
+            set the limit to something smallish so that you can get a result quickly, then do more as needed.
         :return:
         """
         assert self.key, 'You need to apply for the apiKey from http://sp2000.org.cn/api/document'
         fids = {}
         for query in queries:
-            fids[query] = json.loads(requests.get('http://www.sp2000.org.cn/api/family/familyName/familyID/{}/{}'
-                .format(query, self.key)).text)['fids']
+            page_num = limit // 20
+            fid_list = []
+
+            for idx in range(page_num):
+                data = json.loads(requests.get('http://www.sp2000.org.cn/api/v2/getFamiliesByFamilyName',
+                                               params={'familyName': query, 'apiKey': self.key,
+                                                       'page': start + idx}).text)
+                for entry in data['data']['familes']:
+                    fid_list.append(entry['record_id'])
+            fids[query] = fid_list
         return fids
 
-    def search_taxon_id(self, *queries, stype='scientific_name'):
+    def search_taxon_id(self, *queries, name='scientific_name', start = 1, limit = 20):
         """Search taxon IDs
         Search taxon IDs via familyID ,scientificName and commonName.
         :param queries: familyID ,scientificName or commonName. Single or more query.
-        :param stype: stype should in ("familyID","scientificName","commonName"),the default value is "scientificName".
+        :param name: stype should in ("familyID","scientificName","commonName"),the default value is "scientificName".
+        :param start:intenger Record number to start at. If omitted, the results are returned from the first record
+            (start=1). Use in combination with limit to page through results. Note that we do the paging internally for
+            you, but you can manually set the start parameter.
+        :param limit: intenger Number of records to return. This is passed across all sources,when you first query,
+            set the limit to something smallish so that you can get a result quickly, then do more as needed.
         :return:
         """
         assert self.key, ' You need to apply for the apiKey from http://sp2000.org.cn/api/document'
-        assert stype in ['family_id', 'scientific_name', 'common_name'],\
+        assert name in ['family_id', 'scientific_name', 'common_name'],\
             'stype should in ("familyID","scientificName","commonName"),the default value is "scientificName".'
         taxon_id = {}
-        if stype == 'family_id':
-            for fid in queries:
-                taxon_id[fid] = json.loads(requests.get('http://www.sp2000.org.cn/api/taxon/familyID/taxonID/{}/{}'
-                    .format(fid, self.key)).text)['taxonIDs']
+        if name == 'family_id':
+            for query in queries:
+                page_num = limit // 20
+                fid_list = []
 
-        elif stype == 'scientific_name':
-            for fid in queries:
-                taxon_id[fid] = json.loads(requests.get('http://www.sp2000.org.cn/api/taxon/scientificName/taxonID/{}/{}'
-                    .format(fid, self.key)).text)['taxonIDs']
+                for idx in range(page_num):
+                    data = json.loads(requests.get('http://www.sp2000.org.cn/api/v2/getSpeciesByFamilyId',
+                                                   params={'familyId': query, 'apiKey': self.key,
+                                                           'page': start + idx}).text)
+                    if data['data']['species'] is None:
+                        continue
+                    for entry in data['data']['species']:
+                        fid_list.append(entry['namecode'])
+                taxon_id[query] = fid_list
 
-        elif stype == 'common_name':
-            for fid in queries:
-                taxon_id[fid] = json.loads(requests.get('http://www.sp2000.org.cn/api/taxon/commonName/taxonID/{}/{}'
-                    .format(fid, self.key)).text)['taxonIDs']
+        elif name == 'scientific_name':
+            for query in queries:
+                page_num = limit // 20
+                fid_list = []
+
+                for idx in range(page_num):
+                    data = json.loads(requests.get('http://www.sp2000.org.cn/api/v2/getSpeciesByScientificName',
+                                                   params={'scientificName': query, 'apiKey': self.key,
+                                                           'page': start + idx}).text)
+                    if data['data']['species'] is None:
+                        continue
+                    for entry in data['data']['species']:
+                        fid_list.append(entry['accepted_name_info']['namecode'])
+                taxon_id[query] = fid_list
+
+
+        elif name == 'common_name':
+            for query in queries:
+                page_num = limit // 20
+                fid_list = []
+
+                for idx in range(page_num):
+                    data = json.loads(requests.get('http://www.sp2000.org.cn/api/v2/getSpeciesByCommonName',
+                                                   params={'commonName': query, 'apiKey': self.key,
+                                                           'page': start + idx}).text)
+                    if data['data']['species'] is None:
+                        continue
+                    for entry in data['data']['species']:
+                        fid_list.append(entry['accepted_name_info']['namecode'])
+                taxon_id[query] = fid_list
         return taxon_id
 
     def search_checklist(self, *queries):
@@ -68,8 +123,12 @@ class SP2000:
         assert self.key, ' You need to apply for the apiKey from http://sp2000.org.cn/api/document'
         checklist = {}
         for taxon_id in queries:
-            checklist[taxon_id] = json.loads(requests.get(' http://www.sp2000.org.cn/api/taxon/species/taxonID/{}/{}'
-                                    .format(taxon_id, self.key)).text)
+            data  = json.loads(requests.get('http://www.sp2000.org.cn/api/v2/getSpeciesByNameCode',
+                                           params={'nameCode': taxon_id, 'apiKey': self.key}).text)['data']
+            if data is None:
+                continue
+
+            checklist[taxon_id] = data
         return checklist
 
     @staticmethod
@@ -149,25 +208,24 @@ class SP2000:
         return col_taiwan_dict
 
     @staticmethod
-    def get_redlist_china(taxon='', query='', option='Scientific_Names'):
+    def get_redlist_china(query='', option='Scientific Names', group = 'Amphibians'):
         """Query Redlist of Chinese Biodiversity
         Query Redlist of China’s Biodiversity of Vertebrate, Higher Plants and Macrofungi.
-        :param query: The string to query for.
-        :param option: There is one required parameter, which is either Chinese Names or Scientific Names.
-         Give eithera Chinese Names or Scientific Names. If an Scientific Names is given,
-         the Chinese Names parameter may not be used. Only exact matches found the name given will be returned.
-         option should in ("Chinese Names","Scientific Names").
-        :param taxon: There is one required parameter, taxon should in ("Amphibians","Angiospermae","Ascomycetes",
-        "Basidiomycetes","Birds","Bryophyta","Gymnospermae","Inland Fishes","Lichens","Mammals","Pteridophyta",
-        "Reptiles").
+        :param query: string The string to query for.
+        :param option: character There is one required parameter, which is either Chinese Names or Scientific Names.
+            Give eithera Chinese Names or Scientific Names. If an Scientific Names is given, the Chinese Names parameter
+            may not be used. Only exact matches found the name given will be returned. option=c(“Chinese Names”,
+            “Scientific Names”).
+        :param group: character There is one required parameter, group=c(“Amphibians”,“Birds”,“Inland Fishes”,
+            “Mammals”,“Reptiles”,“Plants”,“Fungi”).
         :return: pandas DataFrame
         """
 
-        assert option in ['Chinese_Names','Scientific_Names'], \
-            'option should in ("Chinese_Names","Scientific_Names"),the default value is "Scientific_Names".'
+        assert option in ['Chinese Names','Scientific Names'], \
+            'option should in ("Chinese Names","Scientific Names"),the default value is "Scientific_Names".'
 
-        assert taxon in ["Mammals", "Birds", "Reptiles", "Amphibians", "Inland Fishes", "Plants", "Fungi", ""],\
-            'taxon should in ("Mammals", "Birds", "Reptiles", "Amphibians", "Inland Fishes", "Plants", "Fungi", "")'
+        assert group in ["Amphibians","Birds","Inland Fishes","Mammals","Reptiles","Plants","Fungi", ""],\
+            'taxon should in ("Amphibians","Birds","Inland Fishes","Mammals","Reptiles","Plants","Fungi", "")'
 
         try:
             excel = pd.read_excel(open('RedlistChina.xlsx', 'rb'))
@@ -177,21 +235,26 @@ class SP2000:
                 f.write(data)
             excel = pd.read_excel(open('RedlistChina.xlsx', 'rb'))
 
-        if query and taxon:
+        if query and group:
             if option == 'Chinese_Names':
-                df = excel[(excel['Chinese_Names'] == query) & (excel['Taxon'] == taxon)]
+                df = excel[excel['species_c'].notna()][excel[excel['species_c'].notna()]
+                ['species_c'].str.contains(query, regex=False)]
+                df = df[df['group'] == group]
             else:
-                df = excel[(excel['Scientific_Names'] == query) & (excel['Taxon'] == taxon)]
+                df = excel[excel['species'].notna()][excel[excel['species'].notna()]
+                ['species'].str.contains(query, regex=False)]
+                df = df[df['group'] == group]
         elif query:
-            df = excel[excel['Chinese_Names'] == query]
-        elif taxon:
-            df = excel[excel['Taxon'] == taxon]
+            df = excel[excel['species'].notna()][excel[excel['species'].notna()]
+                ['species'].str.contains(query, regex=False)]
+        elif group:
+            df = excel[excel['group'] == group]
         else:
             df = None
         return df
 
     @staticmethod
-    def get_col_global(*queries, option='name', response='terse', start=0):
+    def get_col_global(*queries, option='name', response='terse', start=0, limit = 500):
         """Search Catalogue of Life Global checklist
         Get Catalogue of Life Global checklist via species name and id.
         :param query: The string to search for.
@@ -208,6 +271,11 @@ class SP2000:
         This is useful if the total number of results is larger than the maximum number of results returned by a single
         Web service query (currently the maximum number of results returned by a single query is 500 for terse queries
         and 50 for full queries,the default value is 0.
+        :param limit: integer Number of records to return. This is useful if the total number of results is larger than
+         the maximum number of results returned by a single Web service query (currently the maximum number of results
+          returned by a single query is 500 for terse queries and 50 for full queries,the default value is 500.Note that
+           there is a hard maximum of 10,000, which is calculated as the limit+start, so start=99,00 and limit=2000
+            won’t work.
         :return:
         """
         assert option in ['name', 'id'], 'option should in (name, id).'
@@ -224,7 +292,7 @@ class SP2000:
             else:
                 result = json.loads(requests.get('http://webservice.catalogueoflife.org/col/webservice?id={}'
                                    '&format=json&response={}&start={}'.format(query, response, start)).text)\
-                    .get('results', None)
+                    .get('results', None)[:limit]
             result_dict[query] = result
         return result_dict
 
@@ -234,7 +302,7 @@ sp2000 = SP2000()
 
 if __name__ == '__main__':
     from pprint import pprint
-    api_key = 'Your Key'
+    api_key = 'null'
 
     sp2000.set_search_key(api_key)
     # print(sp2000.search_taxon_id('1233542354', stype='family_id'))
@@ -247,9 +315,12 @@ if __name__ == '__main__':
     #
     # r = sp2000.get_col_taiwan(query="Anguilla",tree="name",option = "contain")
     # pprint(sp2000.search_checklist('b8c6a086-3d28-4876-8e8a-ca96e667768d'))
-    # print(sp2000.get_col_global(query="Platalea leucorodia", option="name"))
+    # print(len(sp2000.get_col_global("Platalea leucorodia", option="name")['Platalea leucorodia']))
     # print(sp2000.get_col_taiwan("Anguilla", "Anguilla"))
     # print(sp2000.get_col_global("Anguilla marmorata","Anguilla japonica",
     # "Anguilla bicolor","Anguilla nebulosa","Anguilla luzonensis",option="name"))
-    print(sp2000.get_redlist_china(taxon='Inland Fishes'))
+    # print(sp2000.search_taxon_id('Actinidia arg', name = 'scientific_name'))
+    # print(sp2000.search_checklist('123', 'T20171000100267', '123124'))
+    print(sp2000.get_redlist_china(query= 'Anguilla', option = "Scientific Names", group='Inland Fishes'))
+
 
